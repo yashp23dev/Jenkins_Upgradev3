037a68a70a80a92057a0122e852b3d7544ea11ad
PrograMaría <78096795+Progra-maria@users.noreply.github.com>
2024-05-28 19:59:25 +0200

    Update TestGreeter.java
    adds failing test

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -41,4 +41,8 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+  @Test
+  public void intentionallyFail() {
+    assertThat(true, false);
+  }
 }
